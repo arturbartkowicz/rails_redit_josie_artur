0ac36144bc13e17c7f4f7c0b8cfb7b91fae150ca
arturbartkowicz <artur.bartkowicz1@gmail.com>
2019-09-12 13:06:41 +0100

    feat. update adding new pages

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :posts
+  get '/pages', to: 'pages#index'
+  post '/pages', to: 'pages#create'
+  get '/pages/new', to: 'pages#new', as: 'new_page'
+  get '/pages/:id', to: 'pages#show', as: 'page'
+
+  # (UP) In plain English you could read this as, get requests for the /pages path should go to... 
+  # ... the PagestController's index method.
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end

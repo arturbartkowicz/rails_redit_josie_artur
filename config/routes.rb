456218053dcff453928935d6719a86c80c8075d3
arturbartkowicz <artur.bartkowicz1@gmail.com>
2019-09-15 12:11:35 +0100

    feat: update pages form

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   post '/pages', to: 'pages#create'
   get '/pages/new', to: 'pages#new', as: 'new_page'
   get '/pages/:id', to: 'pages#show', as: 'page'
+  get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
+  patch '/pages/:id', to: 'pages#update'
 
   # (UP) In plain English you could read this as, get requests for the /pages path should go to... 
   # ... the PagestController's index method.
